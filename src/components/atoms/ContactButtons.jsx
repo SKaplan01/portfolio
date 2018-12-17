7c0a4b4b47ae7b5e61eccab5cb7b5013ffb72cbd
Sarah Kaplan <skaplan520@gmail.com>
2018-12-17 00:14:19 -0800

    added links to resume github and linkedin

--- src/components/atoms/ContactButtons.jsx
+++ src/components/atoms/ContactButtons.jsx
@@ -1,19 +1,40 @@
 import React, { Component } from 'react'
 import './contactButtons.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'gatsby'
 
 class ContactButtons extends Component {
   render() {
+    const linkStyle = {
+      textDecoration: 'none',
+      textDecorationStyle: 'none',
+      textDecorationLine: 'none',
+      color: 'rgb(206, 206, 206)',
+    }
     return (
-      <div id="contactButtons" style={{ color: 'rgb(206, 206, 206)' }}>
+      <div id="contactButtons">
         <span>
-          <FontAwesomeIcon icon="envelope" />
+          <Link to="/page-2" style={linkStyle}>
+            <FontAwesomeIcon icon="envelope" />
+          </Link>
         </span>
         <span>
-          <FontAwesomeIcon icon={['fab', 'github']} />
+          <a
+            href="https://github.com/skaplan01/"
+            target="_blank"
+            style={{ color: 'rgb(206, 206, 206)', textDecoration: 'none' }}
+          >
+            <FontAwesomeIcon icon={['fab', 'github']} />
+          </a>
         </span>
         <span>
-          <FontAwesomeIcon icon={['fab', 'linkedin']} />
+          <a
+            href="https://www.linkedin.com/in/sarah-kaplan-dev/"
+            target="_blank"
+            style={{ color: 'rgb(206, 206, 206)', textDecoration: 'none' }}
+          >
+            <FontAwesomeIcon icon={['fab', 'linkedin']} />
+          </a>
         </span>
       </div>
     )
--- src/components/atoms/Intro.jsx
+++ src/components/atoms/Intro.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import headshot2 from '../../assets/headshot2.jpg'
+import sarahKaplanResume from '../../assets/sarahKaplanResume.pdf'
 import ContactButtons from './ContactButtons'
 import Button from '@material-ui/core/Button'
 import Icon from '@material-ui/core/Icon'
@@ -14,18 +15,24 @@ class Intro extends Component {
         <h1>Sarah Kaplan</h1>
         <h3>Software Engineer</h3>
         <IntroBlurb />
-        <Button variant="contained" color="primary">
-          <FontAwesomeIcon icon="file-download" color="rgb(206, 206, 206)" />
-          <span
-            style={{
-              paddingLeft: '.7em',
-              letterSpacing: '.15em',
-              color: 'rgb(206, 206, 206)',
-            }}
-          >
-            Resume
-          </span>
-        </Button>
+        <a
+          href={sarahKaplanResume}
+          target="_blank"
+          style={{ textDecoration: 'none' }}
+        >
+          <Button variant="contained" color="primary">
+            <FontAwesomeIcon icon="file-download" color="rgb(206, 206, 206)" />
+            <span
+              style={{
+                paddingLeft: '.7em',
+                letterSpacing: '.15em',
+                color: 'rgb(206, 206, 206)',
+              }}
+            >
+              Resume
+            </span>
+          </Button>
+        </a>
         <ContactButtons />
       </React.Fragment>
     )
